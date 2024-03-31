cabd76a47a45d3092d19f0259ab9447622621be6
Pinthucha <Pinthucha.ruckpintuwat@g.swu.ac.th>
2024-03-31 16:54:03 +0700

    func count likes almost done

--- categorypage.js
+++ categorypage.js
@@ -1,24 +1,16 @@
-
-
-
 window.onload = function() {
-    loadPage(0); // โหลดหน้าแรก
+    loadPage(0);
 };
-
-// ฟังก์ชันสำหรับโหลดเนื้อหาของหน้าที่กำหนด
 function loadPage(pageIndex) {
     const page = pages[pageIndex];
     document.getElementById('content-title').textContent = page.title;
     document.getElementById('content-body').textContent = page.body;
     document.getElementById('content-image').src = page.image;
 }
-
-// เชื่อมโยงลิงก์เมื่อคลิกที่ลิงก์เพื่อโหลดเนื้อหาของหน้าอื่น ๆ
 document.getElementById('link1').addEventListener('click', function() {
-    loadPage(0); // โหลดเนื้อหาของหน้าที่ 1
+    loadPage(0);
 });
-
 document.getElementById('link2').addEventListener('click', function() {
-    loadPage(1); // โหลดเนื้อหาของหน้าที่ 2
+    loadPage(1);
 });
 
